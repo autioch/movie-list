e57aeea9a5c7072c4f6f0c91f7a75435e80d586e
autioch <autioch+github@gmail.com>
2017-02-12 10:24:34 +0100

    Faster startup and reduced bundle size by improving template loader.

--- tools/webpack/tpl-custom-loader.js
+++ tools/webpack/tpl-custom-loader.js
@@ -1,5 +1,22 @@
-const lodashTemplate = require('lodash.template');
+const template = require('lodash.template');
+
+const PREFIX = 'module.exports = ';
+
+const REGEXES = [{
+  test: /(\t|\r\n|\r|\n) */gm,
+  result: ''
+}, {
+  test: / +/gm,
+  result: ' '
+}, {
+  test: /<% %>/gm,
+  result: ''
+}];
+
+const reducer = (text, regex) => text.replace(regex.test, regex.result);
 
 module.exports = function tplCustomLoader(source) {
-  return `var _ = require('lodash.template');\nmodule.exports = ${lodashTemplate(source)}`;
+  this.cacheable();
+
+  return PREFIX + template(REGEXES.reduce(reducer, source)).toString();
 };
