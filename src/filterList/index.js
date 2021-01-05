bd3661f6c4b6075c964a85021b7fd00928cf8fc3
Jakub Szczepaniak <jakub.szczepaniak@mtab.com>
2021-01-06 00:10:44 +0100

    Configuration adjustments.

--- src/filterList/index.js
+++ src/filterList/index.js
@@ -10,30 +10,32 @@ export default function FilterList({ schema, setFilterValue, setSort, sortOrders
   return (
     <div className="filter-list">
       <Title level={3}>Apply filters</Title>
-      {(schema.filters || []).map((filter) => {
-        const { key, type } = filter;
-        const View = TYPE_VIEWS[type];
-        const value = filterValues[key];
-        const label = getLabel(key);
+      {(schema.filters || [])
+        .filter((filter) => !filter.hidden)
+        .map((filter) => {
+          const { key, type } = filter;
+          const View = TYPE_VIEWS[type];
+          const value = filterValues[key];
+          const label = getLabel(key);
 
-        return (
-          <section className="filter" key={key}>
-            <div className="filter-header" onClick={() => setSort(key)} title={`Sort by ${label}`}>
-              <Title level={4}>{label}</Title>
-              <span className={`filter-sort-icon is-sort-${sortOrders[key]}`}></span>
-            </div>
-            <div className="filter-content">
-              <View filterId={key} label={label} value={value} setFilterValue={setFilterValue} items={items}/>
-              {value === undefined ? '' : <Button
-                type="primary"
-                icon={<CloseOutlined />}
-                title={`Reset ${label} filter`}
-                onClick={() => setFilterValue(key, undefined)}
-              />}
-            </div>
-          </section>
-        );
-      })}
+          return (
+            <section className="filter" key={key}>
+              <div className="filter-header" onClick={() => setSort(key)} title={`Sort by ${label}`}>
+                <Title level={4}>{label}</Title>
+                <span className={`filter-sort-icon is-sort-${sortOrders[key]}`}></span>
+              </div>
+              <div className="filter-content">
+                <View filterId={key} label={label} value={value} setFilterValue={setFilterValue} items={items}/>
+                {value === undefined ? '' : <Button
+                  type="primary"
+                  icon={<CloseOutlined />}
+                  title={`Reset ${label} filter`}
+                  onClick={() => setFilterValue(key, undefined)}
+                />}
+              </div>
+            </section>
+          );
+        })}
     </div>
   );
 }
