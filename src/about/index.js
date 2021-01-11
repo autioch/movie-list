8fba93ccfb60aa2276f08327b38caeec0947807e
Jakub Szczepaniak <jakub.szczepaniak@mtab.com>
2021-01-11 19:49:08 +0100

    Filtering notes.

--- src/about/index.js
+++ src/about/index.js
@@ -11,6 +11,9 @@ export default function About() {
       <Title level={4}>App</Title>
       <p>List of movies that I have seen/watched. This is personal list,
       as I wasn't happy with lists provided by IMDB or other similar services.</p>
+      <Title level={4}>Filtering</Title>
+      <p>Clicking on the filter title will sort items by that property.</p>
+      <p>Each applied filter will narrow down possible options of filters, including its own - this is especially useful when give item property has multiple values.</p>
       <Title level={4}>Ranking legend</Title>
       <p>Statistics displayed on the right of each item are colorized based
       on their value.</p>
